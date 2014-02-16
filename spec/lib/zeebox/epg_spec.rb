4eeaf3e2e272aef64d5b068bfffda15ea595d13c
Kieran Andrews <hiddentiger@gmail.com>
2014-02-17 10:19:46 +1030

    Create spec for new epg method.

--- spec/lib/zeebox/epg_spec.rb
+++ spec/lib/zeebox/epg_spec.rb
@@ -4,12 +4,33 @@ describe Zeebox::Epg do
 
   let(:epg) { Zeebox::Epg.new }
 
-  describe "GET epg" do
+  #TODO: Maybe set this in spec helper or similar
+  Zeebox.configure do |config|
+    config.id = test_zeebox_id
+    config.key = test_zeebox_key
+  end
 
-    Zeebox.configure do |config|
-      config.id = test_zeebox_id
-      config.key = test_zeebox_key
-    end    
+  describe "GET epg" do
+    it "must have a epg method" do
+      expect(epg).to respond_to :epg
+    end
+
+    it "must parse the api response from JSON to Array" do
+      expect(epg.epg).to be_a(Array)
+    end
+
+    it "must parse the api response from JSON to Hash" do
+      expect(epg.epg.first).to be_a(Hash)
+    end
+
+
+    it "must perform the request and get the data" do
+      #TODO: make this less fragile and probably use a better rspec matcher
+      expect(epg.epg.any? {|h| h["name"] == "NSW: Sydney"}).to be_true
+    end
+  end
+
+  describe "GET epg regions" do
 
     it "must have a regions method" do
       expect(epg).to respond_to :regions
@@ -21,13 +42,12 @@ describe Zeebox::Epg do
 
     it "must parse the api response from JSON to Hash" do
       expect(epg.regions.first).to be_a(Hash)
-    end   
-
+    end
 
     it "must perform the request and get the data" do
       #TODO: make this less fragile and probably use a better rspec matcher
       expect(epg.regions.any? {|h| h["name"] == "NSW: Sydney"}).to be_true
-      
+
     end
   end
 
