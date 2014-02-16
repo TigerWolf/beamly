460aa2fe5114d5e912d955ffa916d7f15b0c8764
TigerWolf <hiddentiger@gmail.com>
2014-02-16 20:37:17 +1030

    add simple get methods to zeebox epg

--- lib/zeebox.rb
+++ lib/zeebox.rb
@@ -1,7 +1,10 @@
 require 'zeebox/epg'
+require 'curb'
 
 # Ruby toolkit for the Zeebox API
 module Zeebox
+
+  BASE_URI = 'https://api-au.zeebox.com'
 
 
 end
--- lib/zeebox/epg.rb
+++ lib/zeebox/epg.rb
@@ -1,5 +1,11 @@
 module Zeebox
   class Epg
 
+    class << self
+      def get(url)
+        Curl.get(BASE_URI + url)
+      end
+    end
+
   end
 end
