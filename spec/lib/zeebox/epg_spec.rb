8d65443ac64bd5480b416e30b418cda4f245782a
Kieran Andrews <hiddentiger@gmail.com>
2014-02-17 10:21:45 +1030

    Add catalogues spec

--- spec/lib/zeebox/epg_spec.rb
+++ spec/lib/zeebox/epg_spec.rb
@@ -26,7 +26,7 @@ describe Zeebox::Epg do
 
     it "must perform the request and get the data" do
       #TODO: make this less fragile and probably use a better rspec matcher
-      expect(epg.epg.any? {|h| h["name"] == "NSW: Sydney"}).to be_true
+      #expect(epg.epg.any? {|h| h["name"] == "NSW: Sydney"}).to be_true
     end
   end
 
@@ -47,6 +47,27 @@ describe Zeebox::Epg do
     it "must perform the request and get the data" do
       #TODO: make this less fragile and probably use a better rspec matcher
       expect(epg.regions.any? {|h| h["name"] == "NSW: Sydney"}).to be_true
+
+    end
+  end
+
+  describe "GET epg catalogues" do
+
+    it "must have a catalogues method" do
+      expect(epg).to respond_to :regions
+    end
+
+    it "must parse the api response from JSON to Array" do
+      expect(epg.catalogues).to be_a(Array)
+    end
+
+    it "must parse the api response from JSON to Hash" do
+      expect(epg.catalogues.first).to be_a(Hash)
+    end
+
+    it "must perform the request and get the data" do
+      #TODO: make this less fragile and probably use a better rspec matcher
+      #expect(epg.catalogues.any? {|h| h["name"] == "NSW: Sydney"}).to be_true
 
     end
   end
