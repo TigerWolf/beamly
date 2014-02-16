51f70a51b8b705499bd97f3981a5098ea74ad184
TigerWolf <hiddentiger@gmail.com>
2014-02-16 17:58:05 +1030

    add simple failing spec

--- spec/lib/zeebox/epg_spec.rb
+++ spec/lib/zeebox/epg_spec.rb
@@ -6,4 +6,16 @@ describe Zeebox::Epg do
     expect("Yay").to be_a String
   end
 
+  describe "default attributes" do
+
+    it "have api url" do
+      Zeebox::Epg.base_url.must_equal 'https://api-au.zeebox.com'
+    end
+
+    it "have curb methods" do
+      should be_kind_of(Curl)
+    end
+
+  end
+
 end
