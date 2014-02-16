da43ce802a872342501472cca0665e32be141456
Kieran Andrews <hiddentiger@gmail.com>
2014-02-17 10:28:55 +1030

    Update spec to match new requirements.

--- spec/lib/zeebox/epg_spec.rb
+++ spec/lib/zeebox/epg_spec.rb
@@ -16,11 +16,11 @@ describe Zeebox::Epg do
     end
 
     it "must parse the api response from JSON to Array" do
-      expect(epg.epg).to be_a(Array)
+      expect(epg.epg(1)).to be_a(Array)
     end
 
     it "must parse the api response from JSON to Hash" do
-      expect(epg.epg.first).to be_a(Hash)
+      expect(epg.epg(1).first).to be_a(Hash)
     end
 
 
@@ -54,20 +54,21 @@ describe Zeebox::Epg do
   describe "GET epg catalogues" do
 
     it "must have a catalogues method" do
-      expect(epg).to respond_to :regions
+      expect(epg).to respond_to :catalogues
     end
 
     it "must parse the api response from JSON to Array" do
-      expect(epg.catalogues).to be_a(Array)
+      expect(epg.catalogues(48)).to be_a(Array)
     end
 
     it "must parse the api response from JSON to Hash" do
-      expect(epg.catalogues.first).to be_a(Hash)
+      expect(epg.catalogues(48).first).to be_a(Hash)
     end
 
     it "must perform the request and get the data" do
+      binding.pry
       #TODO: make this less fragile and probably use a better rspec matcher
-      #expect(epg.catalogues.any? {|h| h["name"] == "NSW: Sydney"}).to be_true
+      #expect(epg.catalogues(48).any? {|h| h["name"] == "NSW: Sydney"}).to be_true
 
     end
   end
