6fcd1025f85ea7cc6814919589c34037ddb4dd5c
TigerWolf <hiddentiger@gmail.com>
2014-07-01 19:11:41 +0930

    update version

--- lib/beamly/version.rb
+++ lib/beamly/version.rb
@@ -2,10 +2,6 @@ module Beamly
 
   # Current version
   # @return [String]
-<<<<<<< HEAD
   VERSION = "0.0.3".freeze
-=======
-  VERSION = "0.0.2.beta".freeze
->>>>>>> d5c74763cafe43466c2a801efe83c8c9f0f20e5c
 
 end
